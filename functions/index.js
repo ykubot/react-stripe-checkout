9c361197115c0fa993b06d78d1fc6a2131936510
yuichikubota <yuichikubota74@gmail.com>
2019-05-09 01:31:31 +0900

    Add charge function.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,51 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const cors = require('cors')({origin: true});
+const stripe = require('stripe')(functions.config().stripe.secret_token);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+
+const stripeCharge = (request, response) => {
+
+    const body = request.body;
+    const amount = body.charge.amount;
+    const currency = body.charge.currency;
+    const description = 'React Stripe Checkout';
+    const receipt_email = body.token.email;
+    const source = body.token.id;
+    const charge = {amount, currency, description, receipt_email, source};
+
+    stripe.charges.create(charge)
+    .then(res => {
+        send(response, 200, {message: 'success！', result: res});
+        console.log('charge success.');
+    })
+    .catch(err => {
+        send(response, 500, {error: err.message});
+        console.log('charge failed.');
+    });
+}
+
+function send(response, statusCode, body) {
+    response.send({
+      statusCode,
+      headers: { 
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, HEAD, OPTIONS, POST"
+        },
+      data: JSON.stringify(body)
+    });
+}
+
+exports.charge = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+        try {
+            stripeCharge(request, response);
+        } catch (error) {
+            send(response, 500, {error: error.message});
+        }
+    });
+});
+
+
--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
 const stripeApiKey = "pk_test_VZzNAtT2avTk4BW59v0fU0sf";
-const checkoutUrl = "/charge";
+const checkoutUrl = "https://us-central1-react-stripe-checkout.cloudfunctions.net/charge";
+
 
 class Checkout extends Component {
 
     handleToken = (token) => {
-        console.log(token);
         fetch(checkoutUrl, {
             method: "POST",
-            body: JSON.stringify(token),
-            mode: "cors"
+            headers:{
+                'Content-type':'application/json'    
+            },
+            body: JSON.stringify({
+                token, 
+                charge: {
+                    amount: 1000,
+                    currency: 'JPY'
+                }
+            }),
         })
         .then(res => {
             console.log(res);
@@ -35,6 +43,7 @@ class Checkout extends Component {
                 panelLabel="{{amount}}支払う"
                 currency="JPY"
                 locale="ja"
+                allowRememberMe={false}
             />
         );
     }
